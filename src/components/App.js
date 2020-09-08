1d97a836976dd8a5bdb2a545b2d7eb66f67dbead
Talha-Ayub-github <mtalhaayub752@gmail.com>
2020-09-08 19:04:52 +0500

    color is used with consumer-->context object

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -21,9 +22,11 @@ class App extends React.Component {
         <i className="flag nl" onClick={() => this.onLanguageChange("dutch")} />
         <br />
         {/* {this.state.language} */}
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate />
-        </LanguageContext.Provider>
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,35 @@
 import React from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
+// ------------------this.context-------------------------
+// class Button extends React.Component {
+//   static contextType = LanguageContext;
+
+//   render() {
+//     const text = this.context === "english" ? "Submit" : "Voorleggen";
+//     return <button className="ui primary button">{text}</button>;
+//   }
+// }
+
+// -----------------------Consumer -----------------
 class Button extends React.Component {
-  static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "Voorleggen";
+  }
 
   render() {
-    const text = this.context === "english" ? "Submit" : "Voorleggen";
-    return <button className="ui primary button">{text}</button>;
+    return (
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui ${color} button`}>
+            <LanguageContext.Consumer>
+              {(value) => this.renderSubmit(value)}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
   }
 }
-
 export default Button;
